b6dde0ab23ef872fd99a152f52f66a9f06361951
Franca Harzl <harfra21@htl-kaindorf.at>
2024-12-05 14:10:28 +0100

    LanguageContext fixed
    languageContext is working

--- HandyApp/GMapsOfKaindorf/app/_layout.tsx
+++ HandyApp/GMapsOfKaindorf/app/_layout.tsx
@@ -1,37 +1,40 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {useColorScheme} from '@/hooks/useColorScheme';
 import HandwrittenFont from "@/components/HandwrittenFont";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        Peasone: require('../assets/fonts/Peasone.otf'),
+        Super: require('../assets/fonts/Super-Morning.ttf'),
+        Nice: require('../assets/fonts/Simpleness.otf')
+    });
 
-  const [showSplashScreen, setShowSplashScreen] = useState(true);
+    const [showSplashScreen, setShowSplashScreen] = useState(true);
 
-  const finishSplash=()=>{
-    setShowSplashScreen(false)
-  }
+    const finishSplash = () => {
+        setShowSplashScreen(false)
+    }
 
-  return (
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        {loaded ? (
-            showSplashScreen ? (
-                <HandwrittenFont text="HTBLA Kaindorf" finishScreen={finishSplash}/>
-            ) : (
-                <Stack>
-                  <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                  <Stack.Screen name="+not-found" />
-                </Stack>
-            )
-        ) : null}
-      </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            {loaded ? (
+                showSplashScreen ? (
+                    <HandwrittenFont text="HTBLA Kaindorf" finishScreen={finishSplash}/>
+                ) : (
+                    <Stack>
+                        <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                        <Stack.Screen name="+not-found"/>
+                    </Stack>
+                )
+            ) : null}
+        </ThemeProvider>
+    );
 }
