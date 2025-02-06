e1cac4bf5dbc82b8d65db17d3c297d8391670f89
Franca Harzl <harfra21@htl-kaindorf.at>
2025-02-06 15:55:53 +0100

    bitte commands ausführen siehe Discord
    git tracking weg

--- HandyApp/GMapsOfKaindorf/config/server.ts
+++
@@ -1,7 +0,0 @@
-// Die Konfigurationsparameter des Backend-Servers
-//   - Beim Developen sollte die IP-Adresse des Clients genommen werden, wo das Backend gestartet wird.
-//   - Der Port des Backends steht in der bin/www.js Datei vom Express-Server. Dort kann er auch verändert werden.
-export const serverConfig = {
-    ip: '192.168.59.152',
-    port: '3000'
-}
--- backend/config/pgDatabaseInit.ts
+++
@@ -1,9 +0,0 @@
-import {Pool} from "pg";
-
-export const pool: Pool= new Pool({
-    user: 'postgres',
-    host: 'localhosta',
-    database: 'mapsOfKaindorf',
-    password: 'admin',
-    port: 5432
-})
