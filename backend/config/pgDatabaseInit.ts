430d95b37d33f369a320dc475cad724a8f985bea
Franca Harzl <harfra21@htl-kaindorf.at>
2025-02-06 15:39:03 +0100

    gitignore für die config angepasst

--- backend/config/pgDatabaseInit.ts
+++
@@ -1,9 +0,0 @@
-import {Pool} from "pg";
-
-export const pool: Pool= new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'mapsOfKaindorf',
-    password: 'postgres',
-    port: 5432
-})
