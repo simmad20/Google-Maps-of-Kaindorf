15a7f4b00041725126c009f31d9e1af6da95fe66
Pekar201 <simmad20@htl-kaindorf.at>
2025-11-28 13:50:37 +0100

    Small Change

--- HandyApp/MapsOfKaindorf/app/(tabs)/chooseTeacher.tsx
+++ HandyApp/MapsOfKaindorf/app/(tabs)/chooseTeacher.tsx
@@ -1,14 +1,12 @@
-import { Image, StyleSheet, View } from 'react-native';
 import { LanguageContext, LanguageContextType } from '@/components/context/LanguageContext';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
 import TeacherSelection from '@/components/TeacherSelection';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useContext } from 'react';
-
-const logo = require('@/assets/images/logo.png');
 
 export default function ChooseTeacherScreen() {
     const { texts } = useContext<LanguageContextType>(LanguageContext);
@@ -16,12 +14,15 @@ export default function ChooseTeacherScreen() {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <ParallaxScrollView
-                headerBackgroundColor={{ light: '#ffffff', dark: '#2d2929ff' }}
+                headerBackgroundColor={{ light: 'transparent', dark: 'transparent' }}
                 headerImage={
-                    <View style={styles.headerContainer}>
-                        <Image source={logo} style={styles.logo} resizeMode="cover" />
-                    </View>
+                    <ThemedView style={styles.headerTextContainer}>
+                        <ThemedText style={styles.headerTextOuter}>
+                            <ThemedText style={styles.headerText}>Maps of Kaindorf</ThemedText>
+                        </ThemedText>
+                    </ThemedView>
                 }
+                headerHeight={80}
             >
                 <ThemedView style={styles.titleContainer}>
                     <ThemedText type="title">{texts.selectText}</ThemedText>
@@ -35,15 +36,36 @@ export default function ChooseTeacherScreen() {
 const HEADER_HEIGHT = 150;
 
 const styles = StyleSheet.create({
-    headerContainer: { 
+    headerTextContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2d283e',
+        borderBottomLeftRadius: 12,
+        borderBottomRightRadius: 12,
+        borderRightColor: '#a453ec',
+        borderLeftColor: '#a453ec',
+        borderRightWidth: 3,
+        borderLeftWidth: 3,
+        borderBottomWidth: 3,
+        borderBottomColor: '#a453ec',
+    },
+    headerTextOuter: {},
+    headerText: {
+        color: '#a453ec',
+        fontSize: 24,
+        fontFamily: 'Nice',
+    },
+    headerGear: { marginLeft: 20 },
+    headerContainer: {
         width: '100%',
         height: HEADER_HEIGHT,
         justifyContent: 'center',
         alignItems: 'center'
     },
-    logo: { 
+    logo: {
         width: '100%',
-        height: '100%' 
+        height: '100%'
     },
     headerImage: {
         color: '#808080',
