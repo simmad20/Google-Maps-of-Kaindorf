c1c37aa13eaf9d36b5965ea46b99d2a0c221e340
Franca Harzl <harfra21@htl-kaindorf.at>
2025-12-19 11:07:52 +0100

    deleted and fixed
    fixed bug

--- HandyApp/MapsOfKaindorf/components/StyledText.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MonoText = MonoText;
-var Themed_1 = require("./Themed");
-function MonoText(props) {
-    return <Themed_1.Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]}/>;
-}
