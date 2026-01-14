880652048ce382ea519e85f057780029050d978f
Pekar201 <simmad20@htl-kaindorf.at>
2026-01-14 10:47:44 +0100

    Small Change

--- HandyApp/MapsOfKaindorf/config/server.ts
+++ HandyApp/MapsOfKaindorf/config/server.ts
@@ -2,5 +2,5 @@
 //   - Beim Developen sollte die IP-Adresse des Clients genommen werden, wo das Backend gestartet wird.
 //   - Der Port des Backends steht in der bin/www.js Datei vom Express-Server. Dort kann er auch verändert werden.
 export const serverConfig = {
-    dns: 'http://localhost:8080/api',
+    dns: 'kainfind.uber.space/api',
 }
