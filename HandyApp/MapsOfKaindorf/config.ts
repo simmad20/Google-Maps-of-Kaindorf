7843ebfac6ed9d61f6a215c78e81e436ed68533c
ramroz19 <ramroz19@htl-kaindorf.at>
2026-01-20 08:57:28 +0100

    router.d.ts

--- HandyApp/MapsOfKaindorf/config.ts
+++ HandyApp/MapsOfKaindorf/config.ts
@@ -1,6 +1,6 @@
 export const URL_START={
     'PROD': "https://kainfind.uber.space",
-    'DEV': "http://localhost:8080"
+    'DEV': "http://localhost:8081"
 }
 
 export const API_URL = URL_START.DEV+"/api";
